f0a4eaa88c061bfcc11c129b3dc7da13672f4b26
Farid <farid.babazov@gmail.com>
2020-04-15 20:16:43 +0600

    add validation and method to get author name

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :subnews, class_name: "Post",
            foreign_key: "repost_news_id"
-
   belongs_to :repost_news, class_name: "Post", optional: true
+
+  validates :title, presence: true
+
+
+  def author
+    self.user.full_name
+  end
 end
